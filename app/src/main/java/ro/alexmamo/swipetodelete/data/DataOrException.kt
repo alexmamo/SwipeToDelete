09e2e7935005261c46ed960baceafa7db3631314
alexm <alexmamo@gmail.com>
2020-10-29 20:52:09 +0200

    Initial commit

--- app/src/main/java/ro/alexmamo/swipetodelete/data/DataOrException.kt
+++ app/src/main/java/ro/alexmamo/swipetodelete/data/DataOrException.kt
@@ -1,3 +1,6 @@
 package ro.alexmamo.swipetodelete.data
 
-data class DataOrException<T, E : Exception?>(var data: T? = null, var e: E? = null)
+data class DataOrException<T, E : Exception?>(
+    var data: T? = null,
+    var e: E? = null
+)
--- app/src/main/java/ro/alexmamo/swipetodelete/utils/Constants.kt
+++ app/src/main/java/ro/alexmamo/swipetodelete/utils/Constants.kt
@@ -1,7 +1,7 @@
 package ro.alexmamo.swipetodelete.utils
 
 object Constants {
-    const val TAG: String = "SwipeToDeleteTag"
+    const val TAG = "SwipeToDeleteTag"
     const val PRODUCTS_COLLECTION = "products"
     const val NAME_PROPERTY = "name"
 }
