ffe4fc585271780b768b6d722862bde2e526051d
alexm <alexmamo@gmail.com>
2020-11-10 15:39:02 +0200

    Minor fixes and improvements

--- app/src/main/java/ro/alexmamo/swipetodelete/products/ProductsActivity.kt
+++ app/src/main/java/ro/alexmamo/swipetodelete/products/ProductsActivity.kt
@@ -24,7 +24,6 @@ class ProductsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_products)
         dataBinding = setContentView(this, R.layout.activity_products)
         setProductsAdapter()
         getProductList()
